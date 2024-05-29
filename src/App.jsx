312c4af8b3693a492409a2a6ff32f6af41365c56
kidist kifle <kidistkifle31@gmail.com>
2024-05-29 03:37:27 -0700

    update pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import GeneralLayout from "./layout/GeneralLayout";
+import About from "./page/About";
+
+const routers = [
+  {
+    path: "",
+    element: <GeneralLayout />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+];
+
+const router = createBrowserRouter(routers);
 
 function App() {
-  return <h1 className="text-fuchsia-600">Brothers</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
